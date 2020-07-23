09c61107c96cc03d750f538636d82b93e0d7a7ab
kadirermaya <kadir.ermaya@gmail.com>
2020-07-23 12:17:48 -0700

    Function created. Takes a string and reverse it.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict"
+
+let string = document.getElementById("string").innerHTML = "Hello";
+let reversedString = document.getElementById("reversedString").innerHTML = reverseString(string);
+
+
+// takes a string and reverse it
+function reverseString(string)
+{
+    var reversed="";
+    for (let index = string.length-1; index >= 0; index--) 
+    {
+        reversed += string[index];
+    }
+    return reversed;
+}
