0d15007b662c8db834bf43732f98311bd0a6d9a8
kadirermaya <kadir.ermaya@gmail.com>
2020-07-23 12:50:36 -0700

    Function created. Takes string sentence and capitalizes each letter of the sentence

--- script.js
+++ script.js
@@ -7,10 +7,36 @@ let reversedString = document.getElementById("reversedString").innerHTML = rever
 // takes a string and reverse it
 function reverseString(string)
 {
-    var reversed="";
+    let reversed="";
     for (let index = string.length-1; index >= 0; index--) 
     {
         reversed += string[index];
     }
     return reversed;
+}
+
+
+//Capitalize the firs letter of each word
+
+let nonCapitalized = document.getElementById("capitalize").innerHTML = "hello world" 
+let capitalized = document.getElementById("capitalized").innerHTML = capitalizeFirstLetter(nonCapitalized);
+
+function capitalizeFirstLetter(nonCapitalized){
+    let capitalized = "";
+    for (let index = 0; index <= nonCapitalized.length-1; index++) 
+        {
+            if(index == 0){
+                capitalized += nonCapitalized[index].toUpperCase();
+            }
+            else if (nonCapitalized[index] === " "){
+                capitalized += " ";
+                capitalized += nonCapitalized[index + 1].toUpperCase();
+                index++;
+            }
+            else{
+
+                capitalized += nonCapitalized[index];
+            }
+        }
+    return capitalized;
 }
